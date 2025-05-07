d19407b9f593ae1127f7f39119d1d0870abd7cbd
benjiemalinao87 <benjiemalinao87@gmail.com>
2025-05-08 03:42:07 +1000

    updated footer

--- src/pages/LeadBookingPage.tsx
+++ src/pages/LeadBookingPage.tsx
@@ -336,17 +336,11 @@ const LeadBookingPage = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-yellow-400 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
               </svg>
-              <span>Solar Powered Future</span>
-            </div>
-            <div className="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-green-400 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-              </svg>
-              <span>Eco-Friendly Energy</span>
+              <span>Cobalt Power</span>
             </div>
           </div>
           <div className="text-center text-gray-400 text-sm">
-            © {new Date().getFullYear()} Solar Powered Future. All rights reserved.
+            © Cobalt Power. All rights reserved.
           </div>
         </div>
       </footer>
